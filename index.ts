1e91bde1b7681381f48419066483a899c8782ec3
jewel109 <raihanurrahmanjewel@gmail.com>
2024-11-13 18:54:10 +0600

    case study 2 is added

--- index.ts
+++ index.ts
@@ -1,1 +1,61 @@
 
+interface IEmploy {
+  calculateSalary(): number
+}
+class Employ implements IEmploy {
+  private hour
+
+  constructor(hour: number) {
+    this.hour = hour
+  }
+  calculateSalary() {
+    console.log("calculating salary for regular Employ for " + this.hour)
+    return this.hour
+  }
+}
+
+class ContractEmploy implements IEmploy {
+  private fixedRate: number
+  constructor(fixedRate: number) {
+    this.fixedRate = fixedRate
+  }
+  calculateSalary() {
+    console.log("calculating salary for ContractEmploy on fixedRate " + this.fixedRate)
+    return this.fixedRate
+  }
+}
+
+type EmployType = "regular" | "contract"
+class Salary {
+
+  employType?: EmployType
+  hours?: number
+  fixedRate?: number
+  salary?: number
+
+
+  constructor(employType: EmployType, secondArg: number) {
+    if (this.employType == "regular") {
+      this.hours = secondArg
+    } else if (this.employType == "contract") {
+      this.fixedRate = secondArg
+    }
+    else {
+      throw new Error("Invalid employType provided")
+    }
+
+  }
+
+  generateSalary(): number {
+    if (this.employType == "regular") {
+      return this.salary = new Employ(2).calculateSalary()
+    } else {
+      return this.salary = new ContractEmploy(4).calculateSalary()
+
+    }
+  }
+}
+
+const salary = new Salary("regular", 2)
+
+const salary2 = new Salary("contract", 40)
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+# case study id 2
+
+
+Background: A payroll system calculates salaries for different employee types. A general Employee class calculates salary based on hours worked, while ContractEmployee (a derived class) might have a different payment calculation, like a fixed contract rate. The payroll system processes all employee types and expects consistent results.
+
+
+Question: How would you design the Employee and ContractEmployee classes to ensure the payroll system handles both without needing special conditions?
